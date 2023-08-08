08d0e5f0a05de804ebc46ccc7c7021033af21e24
Alen Alduk <youstylez@gmail.com>
2023-08-08 12:24:41 +0200

    added walking sound

--- classes/character.class.js
+++ classes/character.class.js
@@ -10,6 +10,7 @@ class Character extends moveableObject {
   currentImage = 0;
   speed = 10;
   world;
+  walking_sound = new Audio("./audio/walking.mp3");
 
   constructor() {
     super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
@@ -19,14 +20,17 @@ class Character extends moveableObject {
 
   animate() {
     setInterval(() => {
+      this.walking_sound.pause();
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.x += this.speed;
         this.otherDirection = false;
+        this.walking_sound.play();
       }
 
       if (this.world.keyboard.LEFT && this.x > 0) {
         this.x -= this.speed;
         this.otherDirection = true;
+        this.walking_sound.play();
       }
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
