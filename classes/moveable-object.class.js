1ba46ad07fe1491d75eb67e757824ce96c62aa79
Alen Alduk <youstylez@gmail.com>
2023-08-11 22:27:11 +0200

    added Frames around drawings and cleaned up code

--- classes/moveable-object.class.js
+++ classes/moveable-object.class.js
@@ -27,6 +27,18 @@ class moveableObject {
     this.img.src = path;
   }
 
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    ctx.beginPath();
+    ctx.linewidth = "5";
+    ctx.strokeStyle = "blue";
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+
   /**
    *
    * @param {Array} arr - [Image Sources]
@@ -46,9 +58,8 @@ class moveableObject {
   }
 
   moveLeft() {
-      this.x -= this.speed;
-      this.otherDirection = true;
-
+    this.x -= this.speed;
+    this.otherDirection = true;
   }
 
   playAnimation(images) {
